b898a86e50d595b671d038d9a476ca22a59bef71
Soomanche199 <Soomanche199@users.noreply.github.com>
2021-04-12 09:41:07 +0000

    deploy: aa8739fc37f080ce8ad03a3e3ba4641d1a386e28

---
+++ _nuxt/static/1618220464/portfolio/sooman/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1618220464",layout:"error",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{content:{dbHash:"e4cf379d"}}}}("\u002Fportfolio\u002Fsooman"));
